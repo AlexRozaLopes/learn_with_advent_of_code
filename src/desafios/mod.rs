fe05646de9755d378f7154346351ff5a7ea81ea8
AlexRozaLopes <alexrozaestudos@gmail.com>
2024-05-29 15:12:10 -0300

    primeiro desafio

---
+++ src/desafios/mod.rs
@@ -0,0 +1,1 @@
+pub mod desafio_um;
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+pub mod desafios;
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", desafios::desafio_um::desafio_um(include_str!("./dados/desafio_um.txt")))
 }
