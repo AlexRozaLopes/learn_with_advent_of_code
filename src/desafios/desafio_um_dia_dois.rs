13b0081de28667f5458e8f9e7bd1b615b9835bb7
AlexRozaLopes <alexrozaestudos@gmail.com>
2024-06-02 11:30:29 -0300

    desafio finalizado

--- src/desafios/desafio_um_dia_dois.rs
+++ src/desafios/desafio_um_dia_dois.rs
@@ -1,9 +1,15 @@
-use std::ops::Add;
 use crate::modelos::game::Game;
+
+pub fn power_games(dados: &str) -> i32 {
+    let games = split_game(dados);
+    games.iter().map(|game| {
+        game.red * game.green * game.blue
+    }).sum()
+}
 
 pub fn valid_games(dados: &str) -> i32 {
     let games = split_game(dados);
-    games.iter().filter(|game|{
+    games.iter().filter(|game| {
         game.valid == true
     }).map(|game| {
         println!("{game:?}");
@@ -12,41 +18,26 @@ pub fn valid_games(dados: &str) -> i32 {
 }
 
 fn split_game(dados: &str) -> Vec<Game> {
-
     let mut games: Vec<Game> = vec![];
 
     dados.lines().for_each(|linha| {
-        let mut linha_formatada = linha.replace(":", ",").replace(";", "*,");
+        let linha_formatada = linha.replace(":", ",").replace(";", ",");
         let game_formatado: Vec<&str> = linha_formatada.split(",").collect();
 
         let mut game = Game::new(0, 0, 0, 0, true);
 
         for game_conteudo in game_formatado {
             if game_conteudo.contains("Game") {
-                game.id = game.id.add(
-                    find_valor(game_conteudo)
-                );
-            }else if game_conteudo.contains("red") {
-                game.red = game.red.add(
-                    find_valor(game_conteudo)
-                )
-            }else if game_conteudo.contains("green") {
-                game.green = game.green.add(find_valor(game_conteudo))
+                game.id = if game.id < find_valor(game_conteudo) { find_valor(game_conteudo) } else { game.id };
+            } else if game_conteudo.contains("red") {
+                game.red = if game.red < find_valor(game_conteudo) { find_valor(game_conteudo) } else { game.red };
+            } else if game_conteudo.contains("green") {
+                game.green = if game.green < find_valor(game_conteudo) { find_valor(game_conteudo) } else { game.green };
             } else {
-                game.blue = game.blue.add(find_valor(game_conteudo))
+                game.blue = if game.blue < find_valor(game_conteudo) { find_valor(game_conteudo) } else { game.blue };
             };
 
             game.valid = game.red <= 12 && game.green <= 13 && game.blue <= 14 && game.valid == true;
-
-
-            if game_conteudo.contains("*") {
-                game.red = 0;
-                game.green = 0;
-                game.blue = 0
-            };
-            // println!("{game_conteudo}");
-            // println!("{game:?}");
-
         }
         games.push(game);
     });
@@ -60,7 +51,6 @@ fn find_valor(game_conteudo: &str) -> i32 {
         .map(|literal| literal.to_string())
         .reduce(|mut acc, e| {
             acc.push_str(&e);
-           // println!("{acc}");
             acc
         }).unwrap().parse::<i32>().unwrap()
 }
@@ -75,4 +65,16 @@ Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
 
     assert_eq!(8, valid_games(exemplo))
+}
+
+
+#[test]
+fn exemplo_desafio_parte_dois() {
+    let exemplo = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
+
+    assert_eq!(2286, power_games(exemplo))
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use desafios::desafio_um_dia_um::calibration_values;
 use crate::desafios::desafio_dois_dia_um::spelled_out_with_letters;
-use crate::desafios::desafio_um_dia_dois::valid_games;
+use crate::desafios::desafio_um_dia_dois::{power_games, valid_games};
 
 pub mod desafios;
 mod modelos;
@@ -8,5 +8,6 @@ mod modelos;
 fn main() {
     println!("{}", calibration_values(include_str!("dados/desafio_um_dia_um.txt")));
     println!("{}", calibration_values(&*spelled_out_with_letters(include_str!("dados/desafio_dois_dia_um.txt"))));
-    println!("{}", valid_games(include_str!("dados/desafio_um_dia_dois.txt")))
+    println!("{}", valid_games(include_str!("dados/desafio_um_dia_dois.txt")));
+    println!("{}", power_games(include_str!("dados/desafio_um_dia_dois.txt")));
 }
