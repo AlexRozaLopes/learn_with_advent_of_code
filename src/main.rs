ce3fcda630b0c5b3579c531a698a2818fc48025e
AlexRozaLopes <alexrozaestudos@gmail.com>
2024-12-01 17:57:19 -0300

    first commit

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
+use std::ops::Add;
+
 use desafios::desafio_um::calibration_values;
-use crate::desafios::desafio_um::spelled_out_with_letters;
+
 use crate::desafios::desafio_dois::{power_games, valid_games};
 use crate::desafios::desafio_tres::engine_schematic;
+use crate::desafios::desafio_um::spelled_out_with_letters;
 
 pub mod desafios;
 mod modelos;
 
 fn main() {
     println!("{}", calibration_values(include_str!("dados/desafio_um.txt")));
-    println!("{}", calibration_values(&*spelled_out_with_letters(include_str!("dados/desafio_um.txt"))));
+    println!("{}", calibration_values(&spelled_out_with_letters(include_str!("dados/desafio_um.txt"))));
     println!("{}", valid_games(include_str!("dados/desafio_dois.txt")));
     println!("{}", power_games(include_str!("dados/desafio_dois.txt")));
-    engine_schematic()
+    engine_schematic();
 }
